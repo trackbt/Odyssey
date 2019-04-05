2b953c0e5f603956aa18b586079811adaa6de090
Lukas Puchon <lukassos@gmail.com>
2019-04-05 15:46:25 +0200

    frontend - was blank on github pages -> relative link for homepage

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.33cf8c56847ef2b91d6299c8df05b0f4.js"
+  "/Odyssey/frontend/build/precache-manifest.5b02fb4d766dc39c3fa28ec3a089b253.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Odyssey/frontend/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
