9c1079ab2d1d0e8ea932ca04fa16c5f9846245c4
Lukas Puchon <lukassos@gmail.com>
2019-04-13 09:00:59 +0200

    frontend - slight changes relese frontend

---
+++ frontend/build/precache-manifest.da11304325726255b0ee0eb8e45b1539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "097c2db9b1351ff22acd",
+    "url": "/Odyssey/frontend/build/static/js/runtime~main.a24514ab.js"
+  },
+  {
+    "revision": "064052c3372610e415bb",
+    "url": "/Odyssey/frontend/build/static/js/main.72075fc4.chunk.js"
+  },
+  {
+    "revision": "200424a85b8c2c5305f5",
+    "url": "/Odyssey/frontend/build/static/js/2.c1d1d60e.chunk.js"
+  },
+  {
+    "revision": "064052c3372610e415bb",
+    "url": "/Odyssey/frontend/build/static/css/main.227f6286.chunk.css"
+  },
+  {
+    "revision": "0023f5a43266e4ca99d89dae64a074fc",
+    "url": "/Odyssey/frontend/build/index.html"
+  }
+];
